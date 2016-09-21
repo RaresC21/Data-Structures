5837385534a5ed2c055d15cbb5278a2d6ee57a30
Rares Cristian <raresc2105@gmail.com>
2016-09-20 22:43:19 -0400

    Create Include.cpp
    Header file with all of the necessary packages. Also contains certain Macros and typedefs to make life easier

---
+++ Include.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+#include <stdio.h>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+
+typedef long long int lli;
+using namespace std;
+
+#define pb push_back
+#define INT_INF 1000000000
+#define LONG_INF 1000000000000000000
