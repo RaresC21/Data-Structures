7d6075f0e7fb163f822fb7fd371d185059b2b31e
Rares Cristian <Rares Cristian>
2016-11-16 20:31:20 -0500

    Nov16

--- FenwickTree/Waves.cpp
+++ FenwickTree/Waves.cpp
@@ -1,3 +1,15 @@
+/*
+From HackerEarth:
+
+Given a sequence A1 , A2 , A3 .. AN of length N.
+Find total number of wave subsequences having length greater than 1.
+Wave subsequence of sequence A1 , A2 , A3 .. AN is defined as a set of
+integers i1 , i2 .. ik such that Ai1 < Ai2 > Ai3 < Ai4 ....
+or Ai1 > Ai2 < Ai3 > Ai4 ....
+and i1 < i2 < ...< ik.
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
