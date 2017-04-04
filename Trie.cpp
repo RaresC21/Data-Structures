585bc3dd44af1030d3d1f2da376d08ee5b49f1d5
Rares Cristian <Rares Cristian>
2017-04-03 23:01:27 -0400

    Trie Update

--- Trie.cpp
+++ Trie.cpp
@@ -1,52 +1,59 @@
-/*
-Allows for lookup of a particular string out of a large text for example. 
-Runs in time proportional to the length of the string being looked up.
-*/
-
 class Trie {
 
-	// Trie - Data structure of type Tree
-#define ALPHAHBET_SIZE 26
+private:
+
+#define ALPHAHBET_SIZE 27
+
+    struct node {
+        bool is_end;
+        struct node* child[ALPHAHBET_SIZE];
+    }*front, *behind;
+
+    bool search(string word, node* n) {
+        node * current = n;
+        for (unsigned int i = 0; i < word.length(); i++) {
+            if (word[i] == '*') word[i] = (char)((int)'z' + 1);
+            int letter = (int)word[i] - (int) 'a';
+            if (current->child[letter] == NULL)
+                return false;
+            current = current->child[letter];
+        }
+        return current->is_end;
+    }
+
+    void insert(string word, node* n) {
+        node* current = n;
+        for (unsigned int i = 0; i < word.length(); i++) {
+            if (word[i] == '*') word[i] = (char)((int)'z' + 1);
+            int letter = (int)word[i] - (int) 'a';
+            if (current->child[letter] == NULL)
+                current->child[letter] = new node();
+            current = current->child[letter];
+        }
+        current->is_end = true;
+    }
+
+
+
 public:
 
-	struct node {
-		bool is_end;
-		struct node* child[ALPHAHBET_SIZE];
-	}*front, *behind;
+    Trie() {
+        front = new node();
+        front->is_end = false;
+    }
 
-	void init(node* n) {
-		n = new node();
-		n->is_end = false;
-	}
+    void insert(string word) {
+        insert(word, front);
+    }
 
-	void insert(string word, node* n) {
-		node* current = n;
-		for (unsigned int i = 0; i < word.length(); i++) {
-			int letter = (int)word[i] - (int) 'a';
-			if (current->child[letter] == NULL)
-				current->child[letter] = new node();
-			current = current->child[letter];
-		}
-		current->is_end = true;
-	}
-
-	bool search(string word, node* n) {
-		node * current = n;
-		for (unsigned int i = 0; i < word.length(); i++) {
-			int letter = (int)word[i] - (int) 'a';
-			if (current->child[letter] == NULL)
-				return false;
-			current = current->child[letter];
-		}
-		return current->is_end;
-	}
-
-	int main() {
-		insert("hello", front);
-		insert("hi", front);
-		insert("what", front);
-		if (search("hi", front))
-			cout << "TRUE" << "\n";
-		return 0;
-	}
+    bool search(string word) {
+        return search(word, front);
+    }
 };
+
+int main() {
+
+	Trie *t = new Trie();
+
+	return 0;
+}
