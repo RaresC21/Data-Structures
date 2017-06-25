a89c2d6dd68d0a371690e1b7e857d2b2550b7580
Rares Cristian <raresc2105@gmail.com>
2017-06-24 20:14:31 -0400

    Add Function pointer

--- SegmentTrees/SegmentTree.cpp
+++ SegmentTrees/SegmentTree.cpp
@@ -1,37 +1,47 @@
 /*
-Segment Tree. 
+Segment Tree.
 Build. Query. Update.
 
-Given an array of integers, answers queries to find sum of entries in a given range in O(LogN) time. 
-Builds the tree in O(NLogN) time. Also allows for updates. i.e. adding a particular value to a given entry. 
-This is also done in O(LongN) time.
+Given an array of integers, answers queries to find sum of entries in a given range in O(LogN) time.
+Builds the tree in O(N Log N) time. Also allows for updates. i.e. adding a particular value to a given entry.
+This is also done in O(Log N) time.
 
 Lazy Implementation of Segment Tree.
-Allows for updating an entire range of values, not a single element. 
-This only makes the minimum amount of operations needed, and traverses down the tree only when we have a query 
+Allows for updating an entire range of values, not a single element.
+This only makes the minimum amount of operations needed, and traverses down the tree only when we have a query
 explicitly demanding us to do so.
 */
 
-
+#include <vector>
 #define MAX 100 // This can change depending on input size
 
 class SegTree {
 
 private:
 
-	std::vector <int>& A;
+	int N;
+	std::vector<int>& A;
 	int tree[2 * MAX], lazy[2 * MAX];
-
-public:
-
-	SegTree(std::vector<int>& a) : A(a) {
-		memset(tree, 0, sizeof(tree));
-		memset(lazy, 0, sizeof(lazy));
-	}
+	int (*op) (int a, int b);
 
 	void build(int node, int start, int end);
 	void update(int node, int start, int end, int indx, int val);
-	int query(int node, int start, int end, int l, int r);
+	int query(int node, int start, int end, int l, int r, int default_val);
+
+public:
+
+	SegTree(std::vector<int>& a, int (*op)(int a, int b)) : A(a), op(op) {
+		N = A.size();
+		build(1, 1, N);
+	}
+
+	void update(int indx, int val) {
+		update(1, 1, N, indx, val);
+	}
+
+	int query(int l, int r, int default_val) {
+		return query(1, 1, N, l, r, default_val);
+	}
 
 	void lazy_updateRange(int node, int start, int end, int l, int r, int val);
 	int lazy_queryRange(int node, int start, int end, int l, int r);
@@ -48,14 +58,14 @@ void SegTree::build(int node, int start, int end)
 	build(2 * node, start, mid);
 	build(2 * node + 1, mid + 1, end);
 
-	tree[node] = tree[2 * node] + tree[2 * node + 1];
+	tree[node] = (*op)(tree[2 * node], tree[2 * node + 1]);
 }
 
 void SegTree::update(int node, int start, int end, int indx, int val)
 {
 	if (start == end) {
-		A[indx] += val;
-		tree[node] += val;
+		A[indx] = (*op)(A[indx], val);
+		tree[node] = (*op)(tree[node], val);
 		return;
 	}
 
@@ -65,22 +75,23 @@ void SegTree::update(int node, int start, int end, int indx, int val)
 	else
 		update(2 * node + 1, mid + 1, end, indx, val);
 
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	tree[node] = (*op)(tree[node * 2], tree[node * 2 + 1]);
 }
 
-int SegTree::query(int node, int start, int end, int l, int r)
+int SegTree::query(int node, int start, int end, int l, int r, int default_val)
 {
-	if (r < start || l > end) return 0;
+	if (r < start || l > end)
+		return default_val;
 
 	if (l <= start && r >= end) // completely within range.
 		return tree[node];
 
 	//partially in range, partially out.
 	int mid = (start + end) / 2;
-	int p1 = query(node * 2, start, mid, l, r);
-	int p2 = query(node * 2 + 1, mid + 1, end, l, r);
+	int p1 = query(node * 2, start, mid, l, r, default_val);
+	int p2 = query(node * 2 + 1, mid + 1, end, l, r, default_val);
 
-	return p1 + p2;
+	return (*op)(p1, p2);
 }
 
 void SegTree::lazy_updateRange(int node, int start, int end, int l, int r, int val)
