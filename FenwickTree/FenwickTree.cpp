03f6882d83de83388daa1d051c41bc34c7206bb7
Rares Cristian <raresc2105@gmail.com>
2017-06-24 20:18:40 -0400

    BIT Tree. Make it a class

--- FenwickTree/FenwickTree.cpp
+++ FenwickTree/FenwickTree.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int BIT[1000], a[1000];
+class BitTree {
+public:
 
-void update(int x, int val) {
-    for(; x <= N; x += x & -x)
-        BIT[x] += val;
-}
+    int N;
+    int BIT[1000];
 
-int query(int x) {
-    int sum = 0;
-    for(; x > 0; x -= x & -x)
-        sum += BIT[x];
-    return sum;
-}
+    BitTree(int N) : N(N) {
+        memset(BIT, 0, sizeof(BIT));
+    }
+
+    void update(int x, int val) {
+        for(; x <= N; x += x & -x)
+            BIT[x] += val;
+    }
+
+    int query(int x) {
+        int sum = 0;
+        for(; x > 0; x -= x & -x)
+            sum += BIT[x];
+        return sum;
+    }
+};
 
 int main() {
-    cin >> N;
-    int i;
-    for(i = 1; i <= N; i++) {
-        cin >> a[i];
-        update(i, a[i]);
-    }
-    cout << "sum of first 10 elements is " << query(10) << "\n";
-    cout << "sum of all elements in range [2, 7] is " << query(7) - query(2 - 1) << "\n";
+
+    BitTree *bit = new BitTree(10000);
 
     return 0;
 }
